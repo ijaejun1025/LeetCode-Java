ae392ab138f8be6535f611a6026778210abaf20b
JaeJun Lee <154036705+ijaejun1025@users.noreply.github.com>
2023-12-17 09:51:56 -0600

    Add files via upload

---
+++ 876. Middle of the Linked List/src/middle/main.java
@@ -0,0 +1,30 @@
+package middle;
+
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        Solution solution = new Solution();
+		
+        // Example 1
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(2);
+        head1.next.next = new ListNode(3);
+        head1.next.next.next = new ListNode(4);
+        head1.next.next.next.next = new ListNode(5);;
+        
+        ListNode middleNode1 = solution.middleNode(head1);
+        System.out.println(middleNode1.val);
+        
+        // Example 2
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+        head2.next.next.next.next = new ListNode(5);
+        head2.next.next.next.next.next = new ListNode(6);
+        
+        ListNode middleNode2 = solution.middleNode(head2);
+        System.out.println(middleNode2.val);
+    }
+}
